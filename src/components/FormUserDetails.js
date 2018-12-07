c9ca9ecbb9c73e43130836e803e6e4b53cc00356
Mateen kazia <mateenkazia@Mateens-MacBook-Pro.local>
2018-12-06 21:26:57 -0800

    user detail page displaying

--- src/components/FormUserDetails.js
+++ src/components/FormUserDetails.js
@@ -1,30 +1,58 @@
-import React, { Component } from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import AppBar from 'material-ui/AppBar';
-import TextField from 'material-ui/TextField';
-import RaisedButton from 'material-ui/RaisedButton';
-
+import React, { Component } from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import AppBar from "material-ui/AppBar";
+import TextField from "material-ui/TextField";
+import RaisedButton from "material-ui/RaisedButton";
 
 export class FormUserDetails extends Component {
-    continue = e => {
-        e.preventDefault();
-        this.props.nextStep();
-    }
-
-
-
-
+  continue = e => {
+    e.preventDefault();
+    this.props.nextStep();
+  };
 
   render() {
-      const { values } = this.props;
+    const { values, handleChange } = this.props;
     return (
       <MuiThemeProvider>
         <React.Fragment>
-            <AppBar title="Enter User Detail"/>
+          <AppBar title="Enter User Detail" />
+          <TextField
+            hintText="Enter Your First Name"
+            floatingLabelText="First Name"
+            onChange={handleChange("firstName")}
+            defaultValue={values.firstName}
+          />
+          <br />
+          <TextField
+            hintText="Enter Your Last Name"
+            floatingLabelText="Last Name"
+            onChange={handleChange("lastName")}
+            defaultValue={values.firstName}
+          />
+          <br />
+          <TextField
+            hintText="Enter Your First Email"
+            floatingLabelText="Email"
+            onChange={handleChange("email")}
+            defaultValue={values.firstName}
+          />
+           <br />
+          <RaisedButton
+            label="Continue"
+            primary={true}
+            style={styles.button}
+            onClick={this.continue}
+          />
         </React.Fragment>
       </MuiThemeProvider>
-    )
+    );
   }
 }
 
-export default FormUserDetails
+const styles = {
+  button: {
+    margin: 15
+  }
+};
+
+export default FormUserDetails;
